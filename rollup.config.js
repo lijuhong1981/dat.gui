b2da7ffa3195ba7dee3d361124474c23ff4a6e16
李炬宏 <lijuhong@pcitech.com>
2024-12-12 10:22:09 +0800

    更新工程配置

--- rollup.config.js
+++ rollup.config.js
@@ -1,30 +1,36 @@
-import { nodeResolve } from '@rollup/plugin-node-resolve';
-import { terser } from 'rollup-plugin-terser';
+import resolve from 'rollup-plugin-node-resolve';
 import cleanup from 'rollup-plugin-cleanup';
 import sass from 'rollup-plugin-sass';
+import uglify from 'rollup-plugin-uglify';
 
-const output = (file, format, sourcemap) => ({
-    input: './src/dat/index.js',
-    output: {
-        name: 'dat',
-        file,
-        format,
-        sourcemap,
-    },
-    plugins: [
-        nodeResolve({
-            browser: true,
-            preferBuiltins: false
-        }),
-        sass({
-            insert: true,
-            output: 'build/dat.gui.css',
-            options: { outputStyle: 'compressed' }
-        }),
-        !sourcemap ? cleanup() : undefined,
-        !sourcemap ? terser() : undefined,
-    ]
-});
+function output(file, format, sourcemap) {
+    return {
+        input: './src/dat/index.js',
+        output: {
+            name: 'dat',
+            file,
+            format,
+            sourcemap,
+        },
+        plugins: [
+            resolve(),
+            sass({
+                insert: true,
+                output: 'build/dat.gui.css',
+                options: { outputStyle: 'compressed' }
+            }),
+            !sourcemap ? cleanup() : undefined,
+            !sourcemap ? uglify({
+                output: {
+                    // Preserve license commenting in minified build.
+                    comments: function (node, comment) {
+                        return comment.type === 'comment2';
+                    }
+                }
+            }) : undefined,
+        ]
+    }
+};
 
 export default [
     output('./build/dat.gui.js', 'umd', true),
