d986e7eb2efd8d72edc20f1b861739054a5f41d9
lijuhong <lijuhong@pcitech.com>
2025-04-21 12:49:11 +0800

    更新版本至1.0.3
    更新版本至1.0.3

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 # dat.gui
-
 Forked from [dataarts/dat.gui](https://github.com/dataarts/dat.gui), modify and added.
 
 A lightweight graphical user interface for changing variables in JavaScript.
@@ -9,17 +8,14 @@ Get started with dat.GUI by reading the [API documentation](API.md).
 ## Change Log
 
 ### 2024.5.14
-
 GUI下新增.removeAllControllers() .removeAllFolders() .removeAll()方法;
 
 Controller下新增.hide() .show()方法;
 
 ## Packaged Builds
-
 The easiest way to use dat.GUI in your code is by using the built source at `build/dat.gui.min.js`. These built JavaScript files bundle all the necessary dependencies to run dat.GUI.
 
 In your `head` tag, include the following code:
-
 ```html
 <script type="text/javascript" src="dat.gui.min.js"></script>
 ```
@@ -27,22 +23,22 @@ In your `head` tag, include the following code:
 ## Installing from npm
 
 ```bash
-npm install --save @lijuhong1981/dat.gui
+$ npm install --save @lijuhong1981/dat.gui
 ```
 
 ```js
 // CommonJS:
-const dat = require('dat.gui');
+const dat = require('@lijuhong1981/dat.gui');
 
 // ES6:
-import * as dat from 'dat.gui';
+import * as dat from '@lijuhong1981/dat.gui';
 
 const gui = new dat.GUI();
 ```
 
 ## Directory Contents
 
-```text
+```
 ├── build - Compiled source code.
 ├── src - Source files.
 └── tests - Tests.
@@ -52,16 +48,26 @@ const gui = new dat.GUI();
 
 In the terminal, enter the following:
 
-```bash
-npm install
-npm run build
+```
+$ npm install
+$ npm run build
 ```
 
 ## npm scripts
 
 - npm run build - Build development and production version of scripts.
-- npm run build-docs - Build API markdown docment.
+- npm run dev - Build development version of script and watch for changes.
+
 
 ## Working with Content Security Policy
-
 If you're using a server with a Content Security Policy in place that blocks 'unsafe-inline', you will have problems when dat.gui.js tries to inject style information. To get around this, load 'build/dat.gui.css' as an external style sheet.
+
+## Changes
+View the [Change Log](https://github.com/dataarts/dat.gui)
+
+## Thanks
+The following libraries / open-source projects were used in the development of dat.GUI:
+ * [Rollup](https://rollupjs.org)
+ * [Sass](http://sass-lang.com/)
+ * [Node.js](http://nodejs.org/)
+ * [QUnit](https://github.com/jquery/qunit) / [jquery](http://jquery.com/)
--- rollup.config.js
+++ rollup.config.js
@@ -1,40 +1,55 @@
+/**
+ * dat-gui JavaScript Controller Library
+ * https://github.com/dataarts/dat.gui
+ *
+ * Copyright 2011 Data Arts Team, Google Creative Lab
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+import fs from 'fs';
+import path from 'path';
 import resolve from 'rollup-plugin-node-resolve';
 import cleanup from 'rollup-plugin-cleanup';
+import babel from 'rollup-plugin-babel';
 import sass from 'rollup-plugin-sass';
-import uglify from 'rollup-plugin-uglify';
 
-function output(file, format, sourcemap) {
-    return {
-        input: './src/dat/index.js',
-        output: {
-            name: 'dat',
-            file,
-            format,
-            sourcemap,
-        },
-        plugins: [
-            resolve(),
-            sass({
-                insert: true,
-                output: 'build/dat.gui.css',
-                options: { outputStyle: 'compressed' }
-            }),
-            !sourcemap ? cleanup() : undefined,
-            !sourcemap ? uglify({
-                output: {
-                    // Preserve license commenting in minified build.
-                    comments: function (node, comment) {
-                        return comment.type === 'comment2';
-                    }
-                }
-            }) : undefined,
-        ]
-    }
+const banner = fs.readFileSync(path.join(__dirname, 'licenseBanner.txt'));
+
+export default {
+  input: 'src/dat/index.js',
+  output: [{
+    // TODO: Remove default exports, and this line, in v0.8.0.
+    exports: 'named',
+    file: './build/dat.gui.js',
+    format: 'umd',
+    name: 'dat',
+    sourcemap: true,
+    banner: banner
+  }, {
+    file: './build/dat.gui.module.js',
+    format: 'es',
+    sourcemap: true,
+    banner: banner
+  }],
+  watch: {
+    include: 'src/**'
+  },
+  plugins: [
+    resolve(),
+    sass({
+      insert: true,
+      output: 'build/dat.gui.css',
+      options: {outputStyle: 'compressed'}
+    }),
+    babel({
+      plugins: ['external-helpers'],
+      exclude: 'node_modules/**'
+    }),
+    cleanup()
+  ]
 };
-
-export default [
-    output('./build/dat.gui.js', 'umd', true),
-    output('./build/dat.gui.min.js', 'umd', false),
-    output('./build/dat.gui.esm.js', 'esm', true),
-    output('./build/dat.gui.esm.min.js', 'esm', false),
-]
